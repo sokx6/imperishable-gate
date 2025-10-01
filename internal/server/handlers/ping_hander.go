022fb1ccf7979b89b3e688dce100aaf627259ffe
locxl <sokx126@gmail.com>
2025-10-01 10:38:48 +0800

    feat: add HTTP ping handler returning pong for valid requests

---
+++ internal/server/handlers/ping_hander.go
@@ -0,0 +1,24 @@
+package handlers
+
+import (
+	"net/http"
+
+	types "imperishable-gate/internal"
+
+	"github.com/labstack/echo/v4"
+)
+
+func PingHandler(c echo.Context) error {
+	var req types.PingRequest
+
+	if err := c.Bind(&req); err != nil || req.Action != "ping" {
+		return c.JSON(http.StatusBadRequest, map[string]string{
+			"error": "action must be 'ping'",
+		})
+	}
+
+	return c.JSON(http.StatusOK, types.PingResponse{
+		Action:  "ping",
+		Message: "pong",
+	})
+}
