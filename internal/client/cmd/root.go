15d223352c4d235f12dc5d397edfbebb15b7b1db
locxl <sokx126@gmail.com>
2025-10-02 08:43:25 +0800

    refactor: change server address to global flag and introduce Config struct

--- internal/client/cmd/root.go
+++ internal/client/cmd/root.go
@@ -7,14 +7,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var Config struct {
+	Addr string
+}
+
 var rootCmd = &cobra.Command{
 	Use:   "gate",
 	Short: "gate is a CLI link management tool",
 	Long:  "gate is a CLI link management tool . . .",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		Config.Addr, _ = cmd.Flags().GetString("addr")
+		return nil
+	},
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("Welcome to gate CLI tool. Use -h for help.")
 	},
 	Version: "1.0.0",
+}
+
+func init() {
+	rootCmd.PersistentFlags().StringP("addr", "a", "127.0.0.1:8080", "Server address (host:port)")
 }
 
 func Execute() {
