8ebe5e2fb696939b1921720645effe93113ee1c7
locxl <sokx126@gmail.com>
2025-10-02 16:13:50 +0800

    feat(model): update Link to use tag relation and add note field

--- internal/model/link.go
+++ internal/model/link.go
@@ -1,17 +1,9 @@
+// model/link.go 修改版
 package model
 
-import (
-	"github.com/lib/pq"
-)
-
-// Link 是数据库中的链接记录
 type Link struct {
-	Id   uint           `gorm:"primaryKey"`
-	Url  string         `gorm:"not null;uniqueIndex;size:2048"` // 唯一索引并且限制长度
-	Tags pq.StringArray `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型                    // PostgreSQL 的文本数组类型
-}
-
-// TableName 设置表名为 'links'
-func (Link) TableName() string {
-	return "links"
+	ID   uint   `gorm:"primarykey" json:"id"`
+	Url  string `gorm:"not null;uniqueIndex" json:"url"`
+	Tags []Tag  `gorm:"many2many:link_tags;" json:"tags"` // 改为关联 Tags
+	Note string `json:"note"`                             // 备注字段新增
 }
