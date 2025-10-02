263807c627f54d81073d87e9de65ad6d4c736d23
locxl <sokx126@gmail.com>
2025-10-02 13:00:17 +0800

    refactor: store link tags with pq.StringArray and drop names column

--- internal/model/link.go
+++ internal/model/link.go
@@ -1,11 +1,14 @@
 package model
 
+import (
+	"github.com/lib/pq"
+)
+
 // Link 是数据库中的链接记录
 type Link struct {
-	Id    uint     `gorm:"primaryKey"`
-	Url   string   `gorm:"not null;uniqueIndex;size:2048"` // 唯一索引并且限制长度
-	Tags  []string `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型
-	Names []string `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型
+	Id   uint           `gorm:"primaryKey"`
+	Url  string         `gorm:"not null;uniqueIndex;size:2048"` // 唯一索引并且限制长度
+	Tags pq.StringArray `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型                    // PostgreSQL 的文本数组类型
 }
 
 // TableName 设置表名为 'links'
