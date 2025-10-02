8053d2ab960d665f9466d8dc869742c507c5bb45
locxl <sokx126@gmail.com>
2025-10-02 11:18:40 +0800

    feat:modified the fields of the Link structure and added Tags and Names fields

--- internal/model/link.go
+++ internal/model/link.go
@@ -2,8 +2,10 @@ package model
 
 // Link 是数据库中的链接记录
 type Link struct {
-	ID  uint   `gorm:"primaryKey"`
-	URL string `gorm:"not null;uniqueIndex;size:2048"` // 唯一索引并且限制长度
+	Id    uint     `gorm:"primaryKey"`
+	Url   string   `gorm:"not null;uniqueIndex;size:2048"` // 唯一索引并且限制长度
+	Tags  []string `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型
+	Names []string `gorm:"type:text[]"`                    // PostgreSQL 的文本数组类型
 }
 
 // TableName 设置表名为 'links'
