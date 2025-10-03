990c8e5b12a51dc4abd411c62bedb35833ec6312
locxl <sokx126@gmail.com>
2025-10-03 21:06:45 +0800

    feat:add foreign keys and cascade deletion to the link_tags intermediate table

--- internal/model/link_tag.go
+++ internal/model/link_tag.go
@@ -2,5 +2,7 @@ package model
 
 type LinkTag struct {
 	LinkID uint `gorm:"primaryKey"`
+	Link   Link `gorm:"constraint:OnDelete:CASCADE;foreignKey:LinkID"`
 	TagID  uint `gorm:"primaryKey"`
+	Tag    Tag  `gorm:"constraint:OnDelete:CASCADE;foreignKey:TagID"`
 }
