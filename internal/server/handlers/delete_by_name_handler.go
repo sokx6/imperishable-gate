0533878e7e45166da49e34c289c063c875266aba
locxl <sokx126@gmail.com>
2025-10-03 21:54:25 +0800

    feat:implement delete_by_name_handler

---
+++ internal/server/handlers/delete_by_name_handler.go
@@ -0,0 +1,39 @@
+// handlers/delete.go
+
+package handlers
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+
+	types "imperishable-gate/internal"
+	"imperishable-gate/internal/model"
+	"imperishable-gate/internal/server/database"
+)
+
+// DeleteHandler 处理通过查询参数删除链接的请求
+func DeleteByNameHandler(c echo.Context) error {
+
+	// 从查询参数中获取所有 "link=" 参数值
+	name := c.Param("name") // 获取同名多个 query 值
+	if name == "" {
+		fmt.Println("Name is empty")
+		return c.JSON(http.StatusBadRequest, types.InvalidRequestResponse)
+	}
+
+	// 可选：验证每个 link 是否为合法 URL
+
+	var Name model.Name
+
+	if err := database.DB.Where("name = ?", name).Take(&Name).Error; err != nil {
+		return c.JSON(http.StatusNotFound, types.LinkNotFoundResponse)
+	}
+	if err := database.DB.Delete(&model.Link{}, Name.LinkID).Error; err != nil {
+		return c.JSON(http.StatusInternalServerError, types.DatabaseErrorResponse)
+	}
+
+	return c.JSON(http.StatusOK, types.OKResponse)
+
+}
