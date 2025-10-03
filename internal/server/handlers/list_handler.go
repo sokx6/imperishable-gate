58e1270ebd1694d5aed77067ac1e5c47eb287931
locxl <sokx126@gmail.com>
2025-10-03 21:32:39 +0800

    fix:fix the issue where Tag and Name were null when listing from the backend

--- internal/server/handlers/list_handler.go
+++ internal/server/handlers/list_handler.go
@@ -14,17 +14,30 @@ func ListHandler(c echo.Context) error {
 	var links []model.Link
 	fmt.Println("1")
 	// 查询所有记录
-	if err := database.DB.Find(&links).Error; err != nil {
+	if err := database.DB.Preload("Names").Preload("Tags").Find(&links).Error; err != nil {
 		return c.JSON(http.StatusInternalServerError, types.ListResponse{
 			Code:    -1,
 			Message: "Failed to retrieve links",
 		})
 	}
+	var linkList []types.Link
+
+	for _, link := range links {
+		linkList = append(linkList, types.Link{
+			ID:     link.ID,
+			Url:    link.Url,
+			Tags:   ExtractTagNames(link.Tags),
+			Names:  ExtractNames(link.Names),
+			Remark: link.Remark,
+		})
+
+	}
+	fmt.Println(linkList)
 
 	// 返回成功响应
 	return c.JSON(http.StatusOK, types.ListResponse{
 		Code:    0,
 		Message: "Success",
-		Data:    links, // 或返回完整对象数组：links
+		Data:    linkList,
 	})
 }
