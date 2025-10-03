9353696d5d4bbc632aba76f79ba93129f61cb412
locxl <sokx126@gmail.com>
2025-10-03 12:27:52 +0800

    style: clean up Tag struct formatting

--- internal/model/tag.go
+++ internal/model/tag.go
@@ -2,8 +2,7 @@
 package model
 
 type Tag struct {
-	ID   uint   `gorm:"primarykey;autoIncrement" json:"id"`
-	Name string `gorm:"uniqueIndex;not null" json:"name"`
-
-	Links []Link `gorm:"many2many:link_tags;"` // 多对多关系
+	ID    uint   `gorm:"primarykey;autoIncrement" json:"id"`
+	Name  string `gorm:"uniqueIndex;not null" json:"name"`
+	Links []Link `gorm:"many2many:link_tags"` // 多对多关系
 }
