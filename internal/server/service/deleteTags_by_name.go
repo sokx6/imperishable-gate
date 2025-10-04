00188ae9ea0f593e36180effd6b779de0c6742d0
locxl <sokx126@gmail.com>
2025-10-04 16:31:02 +0800

    feat:implement the deleteTags_by_name database function

---
+++ internal/server/service/deleteTags_by_name.go
@@ -0,0 +1,33 @@
+package service
+
+import (
+	"imperishable-gate/internal/model"
+	"imperishable-gate/internal/server/database"
+	"imperishable-gate/internal/server/utils"
+)
+
+func DeleteTagsByName(name string, tags []string) error {
+	id := utils.NameToLinkId(name)
+	if id == 0 {
+		return ErrNameNotFound
+	}
+	var link model.Link
+	if err := database.DB.Preload("Tags").Take(&link, "id = ?", id).Error; err != nil {
+		return ErrDatabase
+	}
+	existingTags := utils.ExtractTagNames(link.Tags)
+	if !utils.ContainsAll(existingTags, tags) {
+		return ErrInvalidRequest
+	}
+
+	var tagsToDelete []model.Tag
+	if err := database.DB.Where("name IN ?", tags).Find(&tagsToDelete).Error; err != nil {
+		return ErrDatabase
+	}
+
+	if err := database.DB.Model(&link).Association("Tags").Delete(&tagsToDelete); err != nil {
+		return ErrDatabase
+	}
+
+	return nil
+}
