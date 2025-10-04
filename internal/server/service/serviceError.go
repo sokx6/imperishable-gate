166442911e928177ed0b2a6ce0f9a019eff04258
locxl <sokx126@gmail.com>
2025-10-04 16:32:20 +0800

    feat:add some service error type

--- internal/server/service/serviceError.go
+++ internal/server/service/serviceError.go
@@ -6,3 +6,6 @@ import (
 
 var ErrNameAlreadyExists = errors.New("name already exists for another link")
 var ErrInvalidRequest = errors.New("invalid request")
+var ErrNameNotFound = errors.New("name not found")
+var ErrDatabase = errors.New("database error")
+var ErrLinkNotFound = errors.New("link not found")
