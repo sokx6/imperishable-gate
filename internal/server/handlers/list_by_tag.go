f51bbc5d136c33901adca9d1e1e04735494c62ec
locxl <sokx126@gmail.com>
2025-10-04 14:44:10 +0800

    feat:implement the ListByTagHandler

---
+++ internal/server/handlers/list_by_tag.go
@@ -0,0 +1,36 @@
+package handlers
+
+import (
+	"errors"
+	types "imperishable-gate/internal"
+	"imperishable-gate/internal/model"
+	"imperishable-gate/internal/server/database"
+	"imperishable-gate/internal/server/utils"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/gorm"
+)
+
+func ListByTagHandler(c echo.Context) error {
+	tagName := c.Param("tag")
+	var tag model.Tag
+	result := database.DB.Preload("Links.Tags").Preload("Links.Names").First(&tag, "name = ?", tagName)
+	if result.Error != nil {
+		if errors.Is(result.Error, gorm.ErrRecordNotFound) {
+			return c.JSON(http.StatusNotFound, types.TagNotFoundResponse)
+		}
+		return c.JSON(http.StatusInternalServerError, types.DatabaseErrorResponse)
+	}
+	linkList := make([]types.Link, 0)
+	for _, link := range tag.Links {
+		linkList = append(linkList, types.Link{
+			ID:     link.ID,
+			Url:    link.Url,
+			Tags:   utils.ExtractTagNames(link.Tags),
+			Names:  utils.ExtractNames(link.Names),
+			Remark: link.Remark,
+		})
+	}
+	return c.JSON(http.StatusOK, linkList)
+}
