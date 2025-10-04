226bb5f8f1e62ec89080e46e8e5b106f7c65a4b6
locxl <sokx126@gmail.com>
2025-10-04 16:30:43 +0800

    feat:implement the deleteTags_by_link database function

---
+++ internal/server/service/deleteTags_by_link.go
@@ -0,0 +1,34 @@
+package service
+
+import (
+	"imperishable-gate/internal/model"
+	"imperishable-gate/internal/server/database"
+	"imperishable-gate/internal/server/utils"
+
+	"gorm.io/gorm"
+)
+
+func DeleteTagsByLink(url string, tags []string) error {
+	var link model.Link
+	if err := database.DB.Preload("Tags").Take(&link, "url = ?", url).Error; err != nil {
+		if err == gorm.ErrRecordNotFound {
+			return ErrLinkNotFound
+		}
+		return ErrDatabase
+	}
+	existingTags := utils.ExtractTagNames(link.Tags)
+	if !utils.ContainsAll(existingTags, tags) {
+		return ErrInvalidRequest
+	}
+
+	var tagsToDelete []model.Tag
+	if err := database.DB.Where("name IN ?", tags).Find(&tagsToDelete).Error; err != nil {
+		return ErrDatabase
+	}
+
+	if err := database.DB.Model(&link).Association("Tags").Delete(&tagsToDelete); err != nil {
+		return ErrDatabase
+	}
+
+	return nil
+}
