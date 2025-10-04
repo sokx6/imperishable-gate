522001c70ecbe97eb3eb2fbc466eeceab55d1877
locxl <sokx126@gmail.com>
2025-10-04 14:46:50 +0800

    refactor:separate database operations from the handler(2)

--- internal/server/handlers/addRemark_by_link_handler.go
+++ internal/server/handlers/addRemark_by_link_handler.go
@@ -1,16 +1,14 @@
 package handlers
 
 import (
-	"errors"
 	"net/http"
 	"net/url"
 
 	"github.com/labstack/echo/v4"
-	"gorm.io/gorm"
 
 	types "imperishable-gate/internal"
-	"imperishable-gate/internal/model"
-	"imperishable-gate/internal/server/database"
+
+	"imperishable-gate/internal/server/service"
 )
 
 func AddRemarkHandler(c echo.Context) error {
@@ -25,24 +23,7 @@ func AddRemarkHandler(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, types.InvalidUrlResponse)
 	}
 
-	var link model.Link
-
-	result := database.DB.Where("url = ?", req.Link).First(&link)
-	// 如果找不到对应的 Link，创建新的 Link 和关联的 Names
-	if errors.Is(result.Error, gorm.ErrRecordNotFound) {
-		link = model.Link{
-			Url:    req.Link,
-			Remark: req.Remark,
-		}
-		// 创建新的 Link
-		if err := database.DB.Create(&link).Error; err != nil {
-			return c.JSON(http.StatusInternalServerError, types.DatabaseErrorResponse)
-		}
-	} else if result.Error != nil { // 其他数据库错误
-		return c.JSON(http.StatusInternalServerError, types.DatabaseErrorResponse)
-	}
-
-	if err := database.DB.Model(link).Update("Remark", req.Remark).Error; err != nil {
+	if err := service.AddRemarkByLink(req.Link, req.Remark); err != nil {
 		return c.JSON(http.StatusInternalServerError, types.DatabaseErrorResponse)
 	}
 
