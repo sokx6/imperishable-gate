31e59d08c67ad03ce0a4cd06e9ac233fa7ba08a6
locxl <sokx126@gmail.com>
2025-10-05 17:07:23 +0800

    feat:add HashPassword function using bcrypt

---
+++ internal/server/utils/hash_password.go
@@ -0,0 +1,12 @@
+package utils
+
+import "golang.org/x/crypto/bcrypt"
+
+// hashPassword 使用 bcrypt 对密码进行哈希
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return "", err
+	}
+	return string(bytes), nil
+}
