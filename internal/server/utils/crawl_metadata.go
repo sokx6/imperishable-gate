33104c03e87fd2e6939def95a19d14f04e887e5c
locxl <sokx126@gmail.com>
2025-10-06 08:08:30 +0800

    feat:add statusCode return value for the crawler function

--- internal/server/utils/crawl_metadata.go
+++ internal/server/utils/crawl_metadata.go
@@ -8,21 +8,26 @@ import (
 	"golang.org/x/net/publicsuffix"
 )
 
-func CrawlMetadata(rawURL string) (title, desc, keywords string) {
+func CrawlMetadata(rawURL string) (title, desc, keywords string, statusCode int) {
 	jar, _ := cookiejar.New(&cookiejar.Options{PublicSuffixList: publicsuffix.List})
 	client := &http.Client{Jar: jar}
 	// 发送带模拟浏览器头部的请求
-	req, _ := http.NewRequest("GET", rawURL, nil)
+	req, err := http.NewRequest("GET", rawURL, nil)
+	if err != nil {
+		return "", "", "", 0
+	}
+	// 模拟浏览器头部
 	req.Header.Set("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36")
 	req.Header.Set("Accept", "text/html,application/xhtml+xml;q=0.9")
 
 	// 执行请求
 	resp, err := client.Do(req)
 	if err != nil || resp.StatusCode != http.StatusOK {
-		return "", "", ""
+		return "", "", "", 0
 	}
 	defer resp.Body.Close()
-
+	// 获取状态码
+	statusCode = resp.StatusCode
 	// 用 goquery 解析
 	doc, _ := goquery.NewDocumentFromReader(resp.Body)
 	title = doc.Find("title").Text()
