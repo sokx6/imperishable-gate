58f701e7cdd52b3c798b06ea1e8868b259158f84
locxl <sokx126@gmail.com>
2025-10-06 14:29:23 +0800

    feat: add username field to RefreshToken model

--- internal/model/refreshtoken.go
+++ internal/model/refreshtoken.go
@@ -5,6 +5,7 @@ import "time"
 type RefreshToken struct {
 	ID        uint      `gorm:"primaryKey"`
 	UserID    uint      `gorm:"not null;index"`
+	Username  string    `gorm:"not null;index"`
 	Token     string    `gorm:"not null;uniqueIndex"`
 	ExpiresAt time.Time `gorm:"not null"`
 	Revoked   bool      `gorm:"default:false"` // 是否被撤回（登出）
