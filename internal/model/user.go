1ab8c3abb6ab710d09bf626e3c1f729f9c3d8fde
locxl <sokx126@gmail.com>
2025-10-07 16:56:25 +0800

    feat: add unique constraint to email and increase username size limit

--- internal/model/user.go
+++ internal/model/user.go
@@ -2,9 +2,9 @@ package model
 
 type User struct {
 	ID       uint   `gorm:"primarykey" json:"id"`
-	Username string `gorm:"not null;uniqueIndex;size:32" json:"username"`
+	Username string `gorm:"not null;uniqueIndex;size:40" json:"username"`
 	Password string `gorm:"not null;size:64" json:"password"`
-	Email    string `gorm:"not null" json:"email"`
+	Email    string `gorm:"not null;uniqueIndex" json:"email"`
 	Links    []Link `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"links"`
 	Tags     []Tag  `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"tags"`
 }
