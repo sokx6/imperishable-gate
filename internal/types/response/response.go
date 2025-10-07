b56a5771405e5052d8b016c87982ec3cc62e4cbb
locxl <sokx126@gmail.com>
2025-10-07 16:57:26 +0800

    refactor: consolidate response types and add RefreshResponse

--- internal/types/response/response.go
+++ internal/types/response/response.go
@@ -1,6 +1,12 @@
 package response
 
+import "imperishable-gate/internal/types/data"
+
 type Response struct {
 	Message string      `json:"message"`
-	Data    interface{} `json:"data,omitempty"`
+	Links   []data.Link `json:"links,omitempty"`
+}
+
+type RefreshResponse struct {
+	AccessToken string `json:"access_token"`
 }
