f7c01ba7b53736862de11c5c95d4466677c47e53
locxl <sokx126@gmail.com>
2025-10-07 08:01:54 +0800

    refactor: rewrite add command with better parameter handling

--- internal/client/cmd/add.go
+++ internal/client/cmd/add.go
@@ -11,55 +11,53 @@ import (
 var addCmd = &cobra.Command{
 	Use:   "add",
 	Short: "Add a new link to the server database",
+	Long: `Add command supports multiple operations:
+  - Add a new link: --link <url>
+  - Add names to a link: --link <url> --name <name1> [--name <name2> ...]
+  - Add tags by link: --link <url> --tag <tag1> [--tag <tag2> ...]
+  - Add tags by name: --name <name> --tag <tag1> [--tag <tag2> ...]
+  - Add remark by link: --link <url> --remark <remark>
+  - Add remark by name: --name <name> --remark <remark>`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Println("Executing add command...")
+
 		// 获取命令行参数
 		link, _ := cmd.Flags().GetString("link")
-		tags, _ := cmd.Flags().GetStringArray("tag")
-		name, _ := cmd.Flags().GetString("name")
+		tags, _ := cmd.Flags().GetStringSlice("tag")
+		names, _ := cmd.Flags().GetStringSlice("name")
 		remark, _ := cmd.Flags().GetString("remark")
-		if link == "" {
-			if name == "" {
-				fmt.Println("Either link or name must be provided.")
-				return fmt.Errorf("either link or name must be provided")
-			}
-			if len(tags) > 0 {
-				fmt.Println("Adding tags to link with name:", name)
-				return add.AddTagsByName(name, tags, addr, accessToken)
-			}
-			if remark != "" {
-				fmt.Println("Adding remark to link with name:", name)
-				return add.AddRemarkByName(name, remark, addr, accessToken)
-			}
-			fmt.Println("No tags or remark to add for the given name.")
-			return fmt.Errorf("no tags or remark to add for the given name")
+
+		// 参数验证：至少需要 link 或 name
+		if link == "" && len(names) == 0 {
+			fmt.Println("Error: Either --link or --name must be provided.")
+			return fmt.Errorf("either --link or --name must be provided")
 		}
 
-		if name == "" {
-			if len(tags) > 0 {
-				fmt.Println("Adding tags to link:", link)
-				return add.AddTagsByLink(link, tags, addr, accessToken)
-			}
-			if remark != "" {
-				fmt.Println("Adding remark to link:", link)
-				return add.AddRemarkByLink(link, remark, addr, accessToken)
-			}
-			fmt.Println("No tags or remark to add for the given link.")
-			return fmt.Errorf("no tags or remark to add for the given link")
+		// 场景1: 只提供了 name (没有 link)
+		if link == "" && len(names) > 0 {
+			return add.HandleAddByName(names, tags, remark, addr, accessToken)
 		}
 
-		fmt.Println("Adding link:", link)
-		fmt.Println("server:", addr)
-		return add.AddLink(link, addr, accessToken)
+		// 场景2: 只提供了 link (没有 name)
+		if link != "" && len(names) == 0 {
+			return add.HandleAddByLink(link, tags, remark, addr, accessToken)
+		}
+
+		// 场景3: 同时提供了 link 和 name - 添加新链接及其名称
+		if link != "" && len(names) > 0 {
+			return add.HandleAddLinkWithNames(link, names, tags, remark, addr, accessToken)
+		}
+
+		return fmt.Errorf("invalid parameter combination")
 	},
 }
 
 // 初始化命令行参数
 func init() {
-	// 为 add 命令添加参数link，用来指定要添加的链接
-	addCmd.Flags().StringP("link", "l", "", "link to add")
-	addCmd.Flags().StringArrayP("tag", "t", []string{}, "tags for the link")
-	addCmd.Flags().StringP("name", "n", "", "name for the link")
-	addCmd.Flags().StringP("remark", "r", "", "remark for the link")
+	// 为 add 命令添加参数
+	addCmd.Flags().StringP("link", "l", "", "Link URL to add or modify")
+	addCmd.Flags().StringSliceP("tag", "t", []string{}, "Tags for the link (can specify multiple)")
+	addCmd.Flags().StringSliceP("name", "n", []string{}, "Name(s) for the link (can specify multiple)")
+	addCmd.Flags().StringP("remark", "r", "", "Remark for the link")
 	rootCmd.AddCommand(addCmd)
 }
