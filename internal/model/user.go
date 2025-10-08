ab45711918e1b0164462ffbf5c88bf14215b1792
locxl <sokx126@gmail.com>
2025-10-08 15:16:04 +0800

    feat: add email verification fields to User model

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,10 +1,15 @@
 package model
 
+import "time"
+
 type User struct {
-	ID       uint   `gorm:"primarykey" json:"id"`
-	Username string `gorm:"not null;uniqueIndex;size:40" json:"username"`
-	Password string `gorm:"not null;size:64" json:"password"`
-	Email    string `gorm:"not null;uniqueIndex" json:"email"`
-	Links    []Link `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"links"`
-	Tags     []Tag  `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"tags"`
+	ID              uint       `gorm:"primarykey" json:"id"`
+	Username        string     `gorm:"not null;uniqueIndex;size:40" json:"username"`
+	Password        string     `gorm:"not null;" json:"password"`
+	Email           string     `gorm:"not null;uniqueIndex" json:"email"`
+	EmailVerified   bool       `gorm:"default:false" json:"email_verified"`
+	EmailVerifiedAt *time.Time `json:"email_verified_at,omitempty"`
+	Links           []Link     `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"links"`
+	Tags            []Tag      `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"tags"`
+	Names           []Name     `gorm:"foreignKey:UserID;constraint:OnDelete:CASCADE;" json:"names"`
 }
