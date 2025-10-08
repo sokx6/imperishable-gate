575adf1c36e4e1660d9f55a04150ade3e0566acc
locxl <sokx126@gmail.com>
2025-10-08 15:16:04 +0800

    feat: add User relation to Name model

--- internal/model/name.go
+++ internal/model/name.go
@@ -5,5 +5,6 @@ type Name struct {
 	UserID uint   `gorm:"not null;uniqueIndex:idx_user_name" json:"user_id"`
 	Name   string `gorm:"not null;uniqueIndex:idx_user_name;size:32" json:"name"`
 	LinkID uint   `gorm:"not null" json:"link_id"`
+	User   User   `gorm:"foreignKey:UserID;" json:"user"`
 	Link   Link   `gorm:"foreignKey:LinkID;" json:"link"`
 }
