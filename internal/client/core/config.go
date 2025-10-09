21f1e24ba712bb50ef6bd4b22e3a882a122f3bf3
locxl <sokx126@gmail.com>
2025-10-09 20:48:51 +0800

    refactor: optimize server address loading priority

--- internal/client/core/config.go
+++ internal/client/core/config.go
@@ -9,12 +9,19 @@ import (
 )
 
 // LoadServerAddr 从命令行参数、环境变量或 .env 文件中加载服务器地址
+// 优先级: 命令行参数 > 系统环境变量 > .env 文件 > 默认值
 func LoadServerAddr(cmd *cobra.Command) string {
 	addr, _ := cmd.Flags().GetString("addr")
 	if addr == "" {
-		// 尝试从环境变量或 .env 文件中加载地址
-		if err := godotenv.Load(); err == nil {
-			// 优先使用 GATE_SERVER_ADDR，兼容 SERVER_ADDR
+		// 先尝试从系统环境变量中获取
+		addr = os.Getenv("GATE_SERVER_ADDR")
+		if addr == "" {
+			addr = os.Getenv("SERVER_ADDR")
+		}
+
+		// 如果系统环境变量中没有,再尝试从 .env 文件加载
+		if addr == "" {
+			_ = godotenv.Load()
 			addr = os.Getenv("GATE_SERVER_ADDR")
 			if addr == "" {
 				addr = os.Getenv("SERVER_ADDR")
@@ -22,7 +29,7 @@ func LoadServerAddr(cmd *cobra.Command) string {
 		}
 
 		if addr == "" {
-			// 如果不能加载地址，使用默认地址
+			// 如果都没有,使用默认地址
 			fmt.Println("Warning: GATE_SERVER_ADDR not set and .env file not found, using default http://localhost:4514")
 			addr = "http://localhost:4514"
 		}
